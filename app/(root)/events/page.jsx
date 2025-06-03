b016444685b07f6d42d49a6a73f5d426476c56c8
jattu8602 <chaurasiyajatin68@gmail.com>
2025-06-03 13:54:09 +0530

    Refactor Gallery component for responsive layout
    - Enhanced the Gallery component to support distinct layouts for desktop and mobile views.
    - Implemented conditional rendering for video and shot display based on screen size.
    - Improved photo arrangement in mobile layout for better user experience.

--- app/(root)/events/page.jsx
+++ app/(root)/events/page.jsx
@@ -1,8 +1,8 @@
 'use client'
-
 import { videos, shots, photos } from '@/constants/gallery'
 import GalleryItem from '@/components/gallery-item'
 import Image from 'next/image'
+
 export default function Gallery() {
   // Group content into blocks
   const blocks = []
@@ -25,56 +25,118 @@ export default function Gallery() {
     <div className="w-full">
       {blocks.map((block, index) => (
         <div key={index} className="w-full">
-          {/* Top row: Video + Shot */}
-          <div className="flex w-full h-[560px]">
-            {!block.flip ? (
-              <>
-                <div className="w-3/4 h-full">
+          {/* Desktop Layout */}
+          <div className="hidden md:block">
+            {/* Top row: Video + Shot */}
+            <div className="flex w-full h-[560px]">
+              {!block.flip ? (
+                <>
+                  <div className="w-3/4 h-full">
+                    <GalleryItem
+                      item={block.video}
+                      type="video"
+                      customClass="!rounded-none !shadow-none !m-0"
+                    />
+                  </div>
+                  <div className="w-1/4 h-full">
+                    <GalleryItem
+                      item={block.shot}
+                      type="shot"
+                      customClass="!rounded-none !shadow-none !m-0"
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="w-1/4 h-full">
+                    <GalleryItem
+                      item={block.shot}
+                      type="shot"
+                      customClass="!rounded-none !shadow-none !m-0"
+                    />
+                  </div>
+                  <div className="w-3/4 h-full">
+                    <GalleryItem
+                      item={block.video}
+                      type="video"
+                      customClass="!rounded-none !shadow-none !m-0"
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+
+            {/* Bottom row: 4 images */}
+            <div className="flex w-full h-[200px]">
+              {block.photos.map((photo, idx) => (
+                <div className="w-1/4 h-full" key={idx}>
                   <GalleryItem
-                    item={block.video}
-                    type="video"
+                    item={photo}
+                    type="photo"
                     customClass="!rounded-none !shadow-none !m-0"
                   />
                 </div>
-                <div className="w-1/4 h-full">
-                  <GalleryItem
-                    item={block.shot}
-                    type="shot"
-                    customClass="!rounded-none !shadow-none !m-0"
-                  />
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="w-1/4 h-full">
-                  <GalleryItem
-                    item={block.shot}
-                    type="shot"
-                    customClass="!rounded-none !shadow-none !m-0"
-                  />
-                </div>
-                <div className="w-3/4 h-full">
-                  <GalleryItem
-                    item={block.video}
-                    type="video"
-                    customClass="!rounded-none !shadow-none !m-0"
-                  />
-                </div>
-              </>
-            )}
+              ))}
+            </div>
           </div>
 
-          {/* Bottom row: 4 images */}
-          <div className="flex w-full h-[200px]">
-            {block.photos.map((photo, idx) => (
-              <div className="w-1/4 h-full" key={idx}>
+          {/* Mobile Layout */}
+          <div className="block md:hidden">
+            {/* Video full width */}
+            <div className="w-full h-[250px]">
+              <GalleryItem
+                item={block.video}
+                type="video"
+                customClass="!rounded-none !shadow-none !m-0"
+              />
+            </div>
+
+            {/* Shot + 2 vertical photos row */}
+            <div className="flex w-full h-[250px]">
+              {/* Shot - 50% width */}
+              <div className="w-1/2 h-full">
                 <GalleryItem
-                  item={photo}
+                  item={block.shot}
+                  type="shot"
+                  customClass="!rounded-none !shadow-none !m-0"
+                />
+              </div>
+              {/* 2 vertical photos - 50% width total */}
+              <div className="w-1/2 h-full flex flex-col">
+                <div className="w-full h-1/2">
+                  <GalleryItem
+                    item={block.photos[0]}
+                    type="photo"
+                    customClass="!rounded-none !shadow-none !m-0"
+                  />
+                </div>
+                <div className="w-full h-1/2">
+                  <GalleryItem
+                    item={block.photos[1]}
+                    type="photo"
+                    customClass="!rounded-none !shadow-none !m-0"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Bottom 2 photos row */}
+            <div className="flex w-full h-[125px]">
+              <div className="w-1/2 h-full">
+                <GalleryItem
+                  item={block.photos[2]}
                   type="photo"
                   customClass="!rounded-none !shadow-none !m-0"
                 />
               </div>
-            ))}
+              <div className="w-1/2 h-full">
+                <GalleryItem
+                  item={block.photos[3]}
+                  type="photo"
+                  customClass="!rounded-none !shadow-none !m-0"
+                />
+              </div>
+            </div>
           </div>
         </div>
       ))}
